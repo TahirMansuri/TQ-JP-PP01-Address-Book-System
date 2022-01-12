1929fb7c459775c7adfa54aa26c8d80476d176e7
TahirMansuri <tprocks786@gmail.com>
2022-01-12 12:23:19 +0530

    MODIFIED: Deleting the Contact from Address Book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -33,15 +33,19 @@ public class AddressBook {
 
     //Method to Show the Contact Details
     public void displayContact() {
-        System.out.println("Contact Details -");
-        System.out.println("First Name : "+contacts.getFirstName());
-        System.out.println("Last Name : "+contacts.getLastname());
-        System.out.println("Address : "+contacts.getAddress());
-        System.out.println("City : "+contacts.getCity());
-        System.out.println("State : "+contacts.getState());
-        System.out.println("Zip Code : "+contacts.getZip());
-        System.out.println("Phone Number : "+contacts.getPhoneNo());
-        System.out.println("EMail ID : "+contacts.getEmailID());
+        if(contacts.getFirstName()==null) {
+            System.out.println("Contact Details Not Available");
+        } else {
+            System.out.println("Contact Details -");
+            System.out.println("First Name : "+contacts.getFirstName());
+            System.out.println("Last Name : "+contacts.getLastname());
+            System.out.println("Address : "+contacts.getAddress());
+            System.out.println("City : "+contacts.getCity());
+            System.out.println("State : "+contacts.getState());
+            System.out.println("Zip Code : "+contacts.getZip());
+            System.out.println("Phone Number : "+contacts.getPhoneNo());
+            System.out.println("EMail ID : "+contacts.getEmailID());
+        }
     }
 
     //Method to Edit the Existing Contact
@@ -68,6 +72,27 @@ public class AddressBook {
             contacts.setPhoneNo(sc.next());
             System.out.println("Enter the EMail ID :");
             contacts.setEmailID(sc.next());
+        }
+    }
+
+    //Method to Delete the Existing Contact
+    public void deleteContact() {
+        //Get First Name to Edit the Contact
+        System.out.println("Enter the First Name : ");
+        String firstName = sc.next();
+
+        //check if the Given User with First Name
+        if(!firstName.equalsIgnoreCase(contacts.getFirstName())) {
+            System.out.println("The Entered Contact Name is Not Available in Address Book");
+        } else {
+            contacts.setFirstName(null);
+            contacts.setLastname(null);
+            contacts.setAddress(null);
+            contacts.setZip(null);
+            contacts.setState(null);
+            contacts.setCity(null);
+            contacts.setEmailID(null);
+            contacts.setPhoneNo(null);
         }
     }
 }
